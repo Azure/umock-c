b75476206649958e4c5151f4d7a5157c1d49119e
Andrei Porumb <anporumb@microsoft.com>
2024-09-17 12:33:33 -0700

    propagate_all_repos anporumb/pchmock

--- inc/umock_c/umock_c_internal.h
+++ inc/umock_c/umock_c_internal.h
@@ -964,12 +964,16 @@ typedef struct MOCK_CALL_METADATA_TAG
     MU_C2(mock_hook_func_type_,name) MU_C2(mock_hook_,name); \
     TRACK_CREATE_FUNC_TYPE MU_C2(track_create_destroy_pair_malloc_,name); \
     TRACK_DESTROY_FUNC_TYPE MU_C2(track_create_destroy_pair_free_,name); \
+    MU_C2(mock_hook_func_type_,name) MU_C2(mock_hook_,name); \
+    TRACK_CREATE_FUNC_TYPE MU_C2(track_create_destroy_pair_malloc_,name); \
+    TRACK_DESTROY_FUNC_TYPE MU_C2(track_create_destroy_pair_free_,name); \
+    PAIRED_HANDLES MU_C2(paired_handles_,name); \
 
 #define MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK_NO_CODE_IMPL(do_returns, return_type, name, ...) \
     MU_C2(mock_hook_func_type_,name) MU_C2(mock_hook_,name) = NULL; \
     TRACK_CREATE_FUNC_TYPE MU_UNUSED_VAR MU_C2(track_create_destroy_pair_malloc_,name) = NULL; \
     TRACK_DESTROY_FUNC_TYPE MU_UNUSED_VAR MU_C2(track_create_destroy_pair_free_,name) = NULL; \
-    static PAIRED_HANDLES MU_UNUSED_VAR MU_C2(paired_handles_,name); \
+    PAIRED_HANDLES MU_UNUSED_VAR MU_C2(paired_handles_,name); \
     static PAIRED_HANDLES* MU_UNUSED_VAR MU_C2(used_paired_handles_,name) = NULL; \
     static const MOCK_CALL_ARG_METADATA MU_C2(mock_call_args_metadata_,name)[MU_IF(MU_COUNT_ARG(__VA_ARGS__), MU_DIV2(MU_COUNT_ARG(__VA_ARGS__)), 1)] \
         = { MU_IF(MU_COUNT_ARG(__VA_ARGS__),,NULL) MU_FOR_EACH_2(FILL_ARG_IN_METADATA, __VA_ARGS__) }; \
