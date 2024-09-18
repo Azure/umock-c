1ce0cd9fa237d5b77adcd3ea233a040648bcde83
Andrei Porumb <anporumb@microsoft.com>
2024-09-18 06:19:05 -0700

    made the test project better

--- tests/umock_c_2_int/test_dependency_mock_pp_c1.c
+++ tests/umock_c_2_int/test_dependency_mock_pp_c1.c
@@ -1,5 +1,11 @@
+#if 1
 /*QWQWQW*/
 #include "test_dependency_mock.c"
 /*WEWEWE*/
+#else
+#include "umock_c/umock_c_internal.h"
+MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK_NO_CODE_DECL(, int, test_dependency_no_args);
+MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK_NO_CODE_IMPL(, int, test_dependency_no_args);
+#endif
 
 /*this is precompilation of "test_dependency_mock.c"*/
--- tests/umock_c_2_int/test_dependency_mock_pp_h2.c
+++ tests/umock_c_2_int/test_dependency_mock_pp_h2.c
@@ -1,5 +1,10 @@
+#if 1
 /*QWQWQW*/
 #include "test_dependency_mock.h"
 /*WEWEWE*/
+#else
+#include "umock_c/umock_c_internal.h"
+MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK_NO_CODE_DECL(, int, test_dependency_no_args);
+#endif
 
 /*this is precompilation of "test_dependency_mock.h"*/
