9cb38908e3205263cafb8e6c3a468c7411b15780
anporumb <anporumb@users.noreply.github.com>
2024-10-11 08:20:55 -0700

    remove INC_FOLER, add_mock_library (#367)
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * propagate_all_repos anporumb/pchmock3
    
    * use latest dependencies 10 Oct 2024
    
    * remove INC_FOLDER
    
    * propagate_all_repos anporumb/pchmock3
    
    * use latest dependencies 10 Oct 2024

--- tests/umock_c_2_int/test_dependency_mock.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#ifndef TEST_DEPENDENCY_MOCK_H
-#define TEST_DEPENDENCY_MOCK_H
-
-#include "test_dependency.h"
-
-#endif /* TEST_DEPENDENCY_H */
--- tests/umock_c_2_int/test_dependency_mock_pp_h2.c
+++
@@ -1,7 +0,0 @@
-
-/*QWQWQW*/
-#define ENABLE_MOCKS
-#define ENABLE_MOCKS_DECL
-#include "test_dependency_mock.h"
-/*WEWEWE*/
-/*this is precompilation of "test_dependency_mock.h"*/
